905b295cc6bba533cbcdc1924d45d100d4b662f8
venkatesh bellale <bellale15dm@gmail.com>
2019-02-24 18:01:48 +0530

    modifed app.py

--- app.py
+++ app.py
@@ -149,8 +149,8 @@ def login():
 
         get_username = request.form['username']
         get_password = request.form['password']
-
-        if auth.query.filter_by(username = get_username,password=get_password):
+        print(">>>:@@",auth.query.filter_by(username = get_username,password=get_password).all())
+        if auth.query.filter_by(username = get_username,password=get_password).all():
             error = "Invalid Credentials"
 
 
